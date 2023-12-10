0a458176fb3f696b0d5e3cbf043910b2ae5b13ec
Carlos Parra Montoya <79164641+carlokos@users.noreply.github.com>
2023-12-10 23:01:37 +0100

    minor changes

--- src/main/java/com/javaschool/OnlineStore/services/UserService.java
+++ src/main/java/com/javaschool/OnlineStore/services/UserService.java
@@ -1,7 +1,6 @@
 package com.javaschool.OnlineStore.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
